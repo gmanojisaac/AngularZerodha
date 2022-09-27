d86206a271a6e078cf7e9ff70fe81e0ff923e463
gmanojisaac <gmanoj.isaac@gmail.com>
2022-09-27 13:11:49 +0530

    Setup Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KiteTicker } from 'kiteconnect-ts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kiteAngular';
+  ticker: KiteTicker = new KiteTicker({
+    apiKey: '909lcbtyglf6ks4o',
+    accessToken: 'QMzRubq2K85tdur6z2s2aEpXcF6tcsrn',
+  });
+
+  mydata=[];
+  constructor(){
+    this.ticker.on('ticks', (ticks: any[]) => {
+
+      console.log('Ticks', ticks[0].lastPrice);
+    });
+    
+    this.ticker.on('connect', () => {
+      const items = [18647042];
+      this.ticker.subscribe(items);
+    });
+    
+    this.ticker.connect();
+  
+  }
+  
+  
 }
